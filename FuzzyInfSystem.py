9d5a75a44de139eb5ddc520990d78251402fbbd2
Luiz <luizgrj@gmail.com>
2020-05-17 12:51:33 -0300

    fim criação de funções de pertinencias do controlador

--- FuzzyInfSystem.py
+++ FuzzyInfSystem.py
@@ -4,29 +4,43 @@ import numpy as np
 import skfuzzy as fuzz
 
 class FuzzyInfSystem:
+    
+    def __init__(self, dFmax, dXmax, precisaoF, precisaoX):
+        self.dFmax = dFmax
+        self.dXmax = dXmax
+        self.precisaoF = precisaoF
+        self.precisaoX = precisaoX
+        self.stepF = self.precisaoF
+        self.stepX = self.precisaoX
+        num = 2
+        while (self.stepF >= self.precisaoF):
+            (self.uni_dis_F, self.stepF) = np.linspace(-dFmax, dFmax, num, endpoint=True, retstep=True, dtype=float)
+            num+=1
+        num = 2
+        while (self.stepX >= self.precisaoX):
+            (self.uni_dis_X, self.stepX)= np.linspace(-dXmax, dXmax, num, endpoint=True, retstep=True, dtype=float)
+            num+=1
+    
+    def pert_funcs_df(self):
+        '''Função cria funções de pertinência triangulares'''
+        ln = fuzz.trimf(self.uni_dis_F, [-self.dFmax, -self.dFmax, -self.dFmax/3])
+        mn = fuzz.trimf(self.uni_dis_F, [-self.dFmax, -self.dFmax/2, 0])
+        sn = fuzz.trimf(self.uni_dis_F, [-self.dFmax/3, -self.dFmax/6, 0])
+        zr = fuzz.trimf(self.uni_dis_F, [-self.dFmax/12, 0, self.dFmax/12])
+        sp = fuzz.trimf(self.uni_dis_F, [0, self.dFmax/6, self.dFmax/3])
+        mp = fuzz.trimf(self.uni_dis_F, [0, self.dFmax/2, self.dFmax])
+        lp = fuzz.trimf(self.uni_dis_F, [self.dFmax/3, self.dFmax, self.dFmax])
+        pert_matrix_df = [ln,mn,sn,zr,sp,mp,lp]
+        return(pert_matrix_df)
 
-
-    def __init__(self, undis_in, undis_out):
-        self.undis_in = undis_in
-        self.undis_out = undis_out
-  
-  # montar  as funções de pertinÊncias das regras
-
-  # Generate fuzzy membership functions
-    '''
-        qual_tt = fuzz.trimf(x_qual,[0, 3, 5])
-        qual_lo = fuzz.trimf(x_qual, [0, 0, 5])
-        qual_md = fuzz.trimf(x_qual, [0, 5, 10])
-        qual_hi = fuzz.trimf(x_qual, [5, 10, 10])
-        serv_lo = fuzz.trimf(x_serv, [0, 0, 5])
-        serv_md = fuzz.trimf(x_serv, [0, 5, 10])
-        serv_hi = fuzz.trimf(x_serv, [5, 10, 10])
-        tip_lo = fuzz.trimf(x_tip, [0, 0, 13])
-        tip_md = fuzz.trimf(x_tip, [0, 13, 25])
-        tip_hi = fuzz.trimf(x_tip, [13, 25, 25])
-    '''
-
-  # usar função "and" como no exemplo abaixo para obter a saida
-  # uni, aux = fuzz.fuzzy_and(x_qual,qual_md,x_qual,qual_tt)
-  # obter o max das funções do passo anterior
-  # deffuzificar com o centroide
+    def pert_funcs_dx(self):
+        '''Função cria funções de pertinência triangulares'''
+        ln = fuzz.trimf(self.uni_dis_X, [-self.dXmax, -self.dXmax, -self.dXmax/3])
+        mn = fuzz.trimf(self.uni_dis_X, [-self.dXmax, -self.dXmax/2, 0])
+        sn = fuzz.trimf(self.uni_dis_X, [-self.dXmax/3, -self.dXmax/6, 0])
+        zr = fuzz.trimf(self.uni_dis_X, [-self.dXmax/12, 0, self.dXmax/12])
+        sp = fuzz.trimf(self.uni_dis_X, [0, self.dXmax/6, self.dXmax/3])
+        mp = fuzz.trimf(self.uni_dis_X, [0, self.dXmax/2, self.dXmax])
+        lp = fuzz.trimf(self.uni_dis_X, [self.dXmax/3, self.dXmax, self.dXmax])
+        pert_matrix_dx = [ln,mn,sn,zr,sp,mp,lp]
+        return(pert_matrix_dx)
---
+++ teste.py
@@ -0,0 +1,5 @@
+from FuzzyInfSystem import *
+
+f = FuzzyInfSystem(0,10)
+
+f.pert_funcs(0,10,5)
